af87e17fd3c8771f1422cfb20761cb3a11e62c18
yangchunjian <1091938307@qq.com>
2016-09-02 21:04:28 +0800

    添加Test类

---
+++ gitfirst/src/gitfirst/Test.java
@@ -0,0 +1,18 @@
+package gitfirst;
+
+/**
+ * @Class_Name: Test
+ * @Create_by: Yang Chunjian
+ * @Create_date: 2016年9月2日
+ * @Last_Edit_by:
+ * @Edit_Description:
+ * @Create_Version: gitfirst 1.0
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
